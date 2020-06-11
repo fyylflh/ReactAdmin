ad047f6af22dac9dd226fae2629976f806920ed2
fyylflh <fyylf_lh@sina.com>
2020-06-12 01:00:48 +0800

    根据antd配置了修改主题颜色的功能

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import { Button ,message} from 'antd'
+import './App.less'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+    handlerClick= ()=>{
+        message.success("success!")
+    }
+    render(){
+        return  <Button type="primary" onClick={this.handlerClick}>Button</Button>
+    }
 }
-
-export default App;
